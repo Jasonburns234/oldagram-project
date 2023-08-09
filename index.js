a5536b1f76a8cb939a1c508821c6fbae9dfc2057
Jason Burns <67003071+Jasonburns234@users.noreply.github.com>
2023-08-09 16:29:28 +0100

    code refractering

--- index.js
+++ index.js
@@ -29,89 +29,45 @@ const posts = [
 ]
 
 
-
-// post1
-const nameElement = document.getElementsByClassName("user-name");
-const userLocationElement = document.getElementsByClassName("user-location");
-const userLikesElement = document.getElementsByClassName("user-likes");
-const usernameElement = document.getElementsByClassName("username-bold");
-const userPostElement = document.getElementsByClassName("users-post");
-const userAvatarElement = document.getElementsByClassName("user-avatar");
-const postImageElement = document.getElementsByClassName("post-image");
-
-
-nameElement[0].textContent = posts[0].name;
-userLocationElement[0].textContent = posts[0].location;
-userLikesElement[0].textContent = posts[0].likes;
-usernameElement[0].textContent = posts[0].username;
-userPostElement[0].textContent = posts[0].comment;
-userAvatarElement[0].src = posts[0].avatar;
-postImageElement[0].src = posts[0].post;
+// DRY 
+function updatePostContent(postIndex, nameClass, locationClass, likesClass, usernameClass, commentClass, avatarClass, postImageClass) {
+  const post = posts[postIndex];
+  const nameElement = document.getElementsByClassName(nameClass);
+  const userLocationElement = document.getElementsByClassName(locationClass);
+  const userLikesElement = document.getElementsByClassName(likesClass);
+  const usernameElement = document.getElementsByClassName(usernameClass);
+  const userPostElement = document.getElementsByClassName(commentClass);
+  const userAvatarElement = document.getElementsByClassName(avatarClass);
+  const postImageElement = document.getElementsByClassName(postImageClass);
 
 
-// post2
-const nameElement2 = document.getElementsByClassName("user-name-two");
-const userLocationElement2 = document.getElementsByClassName("user-location-two");
-const userLikesElement2 = document.getElementsByClassName("user-likes-two");
-const usernameElement2 = document.getElementsByClassName("username-bold-two");
-const userPostElement2 = document.getElementsByClassName("users-post-two");
-const userAvatarElement2 = document.getElementsByClassName("user-avatar-two");
-const postImageElement2 = document.getElementsByClassName("post-image-two");
+  nameElement[0].textContent = post.name;
+  userLocationElement[0].textContent = post.location;
+  userLikesElement[0].textContent = post.likes;
+  usernameElement[0].textContent = post.username;
+  userPostElement[0].textContent = post.comment;
+  userAvatarElement[0].src = post.avatar;
+  postImageElement[0].src = post.post;
+}
+
+updatePostContent(0, "user-name", "user-location", "user-likes", "username-bold", "users-post", "user-avatar", "post-image");
+
+updatePostContent(1, "user-name-two", "user-location-two", "user-likes-two", "username-bold-two", "users-post-two", "user-avatar-two", "post-image-two");
+
+updatePostContent(2, "user-name-three", "user-location-three", "user-likes-three", "username-bold-three", "users-post-three", "user-avatar-three", "post-image-three");
 
 
-nameElement2[0].textContent = posts[1].name;
-userLocationElement2[0].textContent = posts[1].location;
-userLikesElement2[0].textContent = posts[1].likes;
-usernameElement2[0].textContent = posts[1].username;
-userPostElement2[0].textContent = posts[1].comment;
-userAvatarElement2[0].src = posts[1].avatar;
-postImageElement2[0].src = posts[1].post;
+function setupLikeEvent(postIndex, likeCounterId, likeIconId) {
+  const likeCounter = document.getElementById(likeCounterId);
+  const likeIcon = document.getElementById(likeIconId);
 
 
-// post3
-const nameElement3 = document.getElementsByClassName("user-name-three");
-const userLocationElement3 = document.getElementsByClassName("user-location-three");
-const userLikesElement3 = document.getElementsByClassName("user-likes-three");
-const usernameElement3 = document.getElementsByClassName("username-bold-three");
-const userPostElement3 = document.getElementsByClassName("users-post-three");
-const userAvatarElement3 = document.getElementsByClassName("user-avatar-three");
-const postImageElement3 = document.getElementsByClassName("post-image-three");
-
-
-nameElement3[0].textContent = posts[2].name;
-userLocationElement3[0].textContent = posts[2].location;
-userLikesElement3[0].textContent = posts[2].likes;
-usernameElement3[0].textContent = posts[2].username;
-userPostElement3[0].textContent = posts[2].comment;
-userAvatarElement3[0].src = posts[2].avatar;
-postImageElement3[0].src = posts[2].post;
-
-  // Get references to the elements for like counter
-  const likeCounter = document.getElementById('like-counter');
-  const likeIcon = document.getElementById('like-icon');
-
-  // Add a click event listener to the 1st post like icon
   likeIcon.addEventListener('click', function() {
-    posts[0].likes ++; // Increment the like count
-    likeCounter.textContent = posts[0].likes; // Update the displayed like count
+    posts[postIndex].likes++;
+    likeCounter.textContent = posts[postIndex].likes;
   });
-  
-    // Get references to the 2nd elements
-  const likeCounter1 = document.getElementById('like-counter-two');
-  const likeIcon1 = document.getElementById('like-icon-two');
+}
 
-  // Add a click event listener to the 1st post like icon
-  likeIcon1.addEventListener('click', function() {
-    posts[1].likes ++; // Increment the like count
-    likeCounter1.textContent = posts[1].likes; // Update the displayed like count
-  });
-  
-    // Get references to the 3rd elements
-  const likeCounter2 = document.getElementById('like-counter-three');
-  const likeIcon2 = document.getElementById('like-icon-three');
-
-  // Add a click event listener to the 1st post like icon
-  likeIcon2.addEventListener('click', function() {
-    posts[2].likes ++; // Increment the like count
-    likeCounter2.textContent = posts[2].likes; // Update the displayed like count
-  });
+setupLikeEvent(0, 'like-counter', 'like-icon');
+setupLikeEvent(1, 'like-counter-two', 'like-icon-two');
+setupLikeEvent(2, 'like-counter-three', 'like-icon-three');
